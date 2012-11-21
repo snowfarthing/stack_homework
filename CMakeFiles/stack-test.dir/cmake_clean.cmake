f096090565a0f3daa70da03860af36b64c8db5ec
Alpheus Madsen <alpheus.madsen@gmail.com>
2012-11-21 12:48:14 -0700

    Add CMakeFiles directory.
    Added CMakeFiles directory, hoping that this will be sufficient
    for getting things to work.

---
+++ CMakeFiles/stack-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stack-test.dir/stack-test.c.o"
+  "CMakeFiles/stack-test.dir/stack.c.o"
+  "stack-test.pdb"
+  "stack-test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/stack-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/stack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stack.dir/stack.c.o"
+  "libstack.pdb"
+  "libstack.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
